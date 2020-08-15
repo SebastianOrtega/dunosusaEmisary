4435c8710821e6abac53f7e8c422f1f2a1eee083
SebastianOrtega <sortega@moviltrack.com>
2020-08-15 00:07:52 -0500

    mejoras en reporte

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,11 @@ const { response } = require("express");
 
 module.exports = function (app) {
   app.post("/", function (req, res) {
+    console.log("*****************JSON NUEVO*********************");
     console.log(req.body);
+    console.log("-------------------Completo---------------------");
+    console.log(JSON.stringify(req.body));
+    console.log("************************************************");
     res.sendStatus(200);
   });
 };
