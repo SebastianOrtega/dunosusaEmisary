148a189075a7a77c66ef8762bcd97cc5871167a6
SebastianOrtega <sortega@moviltrack.com>
2020-08-17 10:29:22 -0500

    compactando en un solo archivo

--- server.js
+++ server.js
@@ -6,7 +6,15 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-require("./routes/apiRoutes")(app);
+
+app.post("/", function (req, res) {
+  console.log("*****************JSON NUEVO*********************");
+  console.log(req.body);
+  console.log("-------------------Completo---------------------");
+  console.log(JSON.stringify(req.body));
+  console.log("************************************************");
+  res.sendStatus(200);
+});
 
 app.listen(PORT, function () {
   console.log("Esperando POST: " + PORT);
