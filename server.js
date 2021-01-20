4f423523c074e98fd8be416088fbd8050c6d034c
Sebastian <sortega@moviltrack.com>
2021-01-20 09:05:18 -0600

    datos parseados

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+//require("body-parser-xml")(bodyParser);
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(express.urlencoded({ extended: true }));
+//app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post("/", function (req, res) {
-  console.log("*****************JSON NUEVO*********************");
-  console.log(req.body);
-  console.log("-------------------Completo---------------------");
-  console.log(JSON.stringify(req.body));
+app.use(bodyParser.text({ type: "text/*" }));
+
+app.post("*", function (req, res) {
   console.log("************************************************");
+  //console.log(req.body);
+  let nuevo = req.body.replace(/[\n\r]+/g, "");
+  let array = nuevo.split(";");
+  //console.log("ultimo", array[array.length]);
+  array[array.length - 1] = array[array.length - 1].replace("#IOs", "");
+  array[array.length - 1] = '{"IOS":"' + array[array.length - 1] + '"}';
+  //console.log(array);
+
+  array.map((dato) => console.log(JSON.parse(dato)));
+  console.log("------------------------------------------------");
+  // console.log("************************************************");
   res.sendStatus(200);
 });
 let n = 0;
-setInterval(contador, 1000);
+//setInterval(contador, 1000);
 
 app.listen(PORT, function () {
   console.log("Esperando POST: " + PORT);
