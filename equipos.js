4ae104e663f2d5e94680649bcf3cde00fe2d0b32
SebastianOrtega <sortega@moviltrack.com>
2021-01-28 08:37:41 -0600

    ejemplos de json a enviar

---
+++ equipos.js
@@ -0,0 +1,5 @@
+module.exports = {
+  Equipo1: ["Anden7", "Anden8", "NoConectado", "NoConectado"],
+  Equipo2: ["Anden11", "Anden12", "Anden13", "Anden14"],
+  Equipo3: ["Anden15", "Anden16", "Anden17", "Anden18"],
+};
--- server.js
+++ server.js
@@ -3,17 +3,21 @@ var bodyParser = require("body-parser");
 //require("body-parser-xml")(bodyParser);
 var app = express();
 var PORT = process.env.PORT || 8080;
+const Equipos = require("./equipos.js");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //app.use(bodyParser.urlencoded({ extended: false }));
-
+//console.log("Equipos : ", Equipos.Equipo3[0]);
 //app.use(bodyParser.text({ type: "text/*" }));
 
 app.post("*", function (req, res) {
   console.log("************************************************");
   console.log(Math.floor(Math.random() * 1000));
-  console.log(req.body);
+  const body = req.body;
+  console.log(body);
+  //console.log(Equipos[body.Equipo[]]);
+
   /* let nuevo = req.body.replace(/[\n\r]+/g, "");
   let array = nuevo.split(";");
   //console.log("ultimo", array[array.length]);
@@ -22,6 +26,25 @@ app.post("*", function (req, res) {
   //console.log(array);
 
   array.map((dato) => console.log(JSON.parse(dato))); */
+  console.log("···············································");
+
+  let dato_a_enviar = {
+    location: "Equipos.req.body.Equipo",
+    date: 1611687247620,
+    tagcount: "1",
+    tags: [
+      {
+        rssi: "-43.5",
+        logicaldevice: "Anden7",
+        count: "27",
+        epc: "313030303030303030303031",
+        fields: {
+          TipoTag: "Contenedor",
+        },
+      },
+    ],
+  };
+  console.log("Resultado: ", dato_a_enviar);
   console.log("------------------------------------------------");
   // console.log("************************************************");
   res.sendStatus(200);
