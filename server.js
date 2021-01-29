0b10e9a09debbc677e0fd947f2df21cdfbbe055b
SebastianOrtega <sortega@moviltrack.com>
2021-01-28 21:39:36 -0600

    trabajando en JSON de salida

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
+const _ = require("loadsh");
 //require("body-parser-xml")(bodyParser);
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 const Equipos = require("./equipos.js");
 
 app.use(express.urlencoded({ extended: true }));
@@ -39,34 +40,39 @@ app.post("*", function (req, res) {
       Antena[3].push(element);
     }
   }
-  console.log("Antena: ", Antena);
+  console.log("Antena: ", Antena.length);
   for (let index = 0; index < Antena.length; index++) {
     const element = Antena[index];
+
     if (element.length !== 0)
-      console.log("Elemento" + index + ": " + element.length);
+      console.log("Elemento " + index + ": " + element.length);
+    const arregloTags = [];
     element.map((tag) => {
-      console.log(tag);
+      Anden = Equipos[String(tag[4])][String(tag[5])];
+      //console.log(tag);
+      const objeto = {};
+      let TipoTag = "";
+
+      String(tag[0]).startsWith("3130")
+        ? (TipoTag = "Contenedor")
+        : (TipoTag = "Camion");
+
+      _.assign(objeto, {
+        rssi: tag[3],
+        logicaldevice: Anden,
+        count: tag[2],
+        epc: tag[0],
+        fields: { TipoTag },
+      });
+
+      arregloTags.push(objeto);
+
       //console.log("TAG: ", tag[4], tag[5]);
       //console.log("Date: ", tag[1]);
-      Anden = Equipos[String(tag[4])][String(tag[5])];
-      const JSONxAntena = {
-        location: Anden,
-        date: 0,
-        tagcount: String(element.length),
-        tags: [
-          {
-            rssi: "-43.5",
-            logicaldevice: "Anden7",
-            count: "27",
-            epc: "313030303030303030303031",
-            fields: {
-              TipoTag: "Contenedor",
-            },
-          },
-        ],
-      };
+
       //console.log(JSONxAntena);
     });
+    console.log("ArregloTags", arregloTags);
   }
 
   console.log("------------------------------------------------");
@@ -83,3 +89,20 @@ app.listen(PORT, function () {
 function contador() {
   console.log(n++);
 }
+
+/* const JSONxAntena = {
+  location: Anden,
+  date: 0,
+  tagcount: String(element.length),
+  tags: [
+    {
+      rssi: "-43.5",
+      logicaldevice: "Anden7",
+      count: "27",
+      epc: "313030303030303030303031",
+      fields: {
+        TipoTag: "Contenedor",
+      },
+    },
+  ],
+}; */
